d32f704c4c147d6054dba58f0099dafef19f9027
W3415 <W3415@lab.aca>
2020-02-18 18:17:19 -0500

    comit

---
+++ calculador_test.ts
@@ -0,0 +1,12 @@
+import {Calculadora} from "./calculadora"
+
+describe("calculadora",()=>{
+    it("suma correcta", ()=>{
+        let resEsperado=5
+        let calc= new Calculadora()
+        let resObtenido=calc.sumar(3,2)
+
+        expect(resEsperado).toEqual(resObtenido)
+    })
+
+})
